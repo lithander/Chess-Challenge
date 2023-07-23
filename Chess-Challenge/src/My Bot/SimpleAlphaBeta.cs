0282d624466ec5732a49662deb2968220f223f43
lithander <lithander@gmx.de>
2023-07-23 13:23:24 +0200

    Besides the already existing SimpleNegaMax bot I added a SimpleAlphaBeta bot that does play to a depth of 6 instead of 4, handles draws and does alpha beta pruning.

---
+++ Chess-Challenge/src/My Bot/SimpleAlphaBeta.cs
@@ -0,0 +1,68 @@
+﻿using ChessChallenge.API;
+
+public class SimpleAlphaBeta : IChessBot
+{
+    // Piece values: null, pawn, knight, bishop, rook, queen
+    int[] PieceValues = { 0, 100, 300, 300, 500, 900 };
+    int CheckmateScore = 9999;
+    int Depth = 6;
+
+    public Move Think(Board board, Timer timer)
+    {
+        Move bestMove = default;
+        int alpha = -CheckmateScore;
+        int beta = CheckmateScore;
+        foreach (Move move in board.GetLegalMoves())
+        {
+            board.MakeMove(move);
+            int score = -NegaAlphaBeta(-beta, -alpha, 1, board);
+            board.UndoMove(move);
+
+            if (score > alpha)
+            {
+                bestMove = move;
+                alpha = score;
+            }
+        }
+        return bestMove;
+    }
+
+    private int NegaAlphaBeta(int alpha, int beta, int depth, Board board)
+    {
+        if (depth == Depth)
+            return Eval(board);
+
+        if (board.IsDraw())
+            return 0;
+
+        if (board.IsInCheckmate())
+            return -CheckmateScore;
+
+        foreach (Move move in board.GetLegalMoves())
+        {
+            board.MakeMove(move);
+            int score = -NegaAlphaBeta(-beta, -alpha, depth + 1, board);
+            board.UndoMove(move);
+
+            if (score > alpha)
+            {
+                alpha = score;
+                if (score >= beta)
+                    return beta;
+            }
+        }
+        return alpha;
+    }
+
+    private int Eval(Board board)
+    {
+        int eval = 0;
+        for (int pieceType = 1; pieceType < 5; pieceType++)
+        {
+            var white = board.GetPieceList((PieceType)pieceType, true);
+            var black = board.GetPieceList((PieceType)pieceType, false);
+            eval += (white.Count - black.Count) * PieceValues[pieceType];
+        }
+        return board.IsWhiteToMove ? eval : -eval;
+    }
+}
